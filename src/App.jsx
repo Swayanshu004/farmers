6fe650166642dcac4f0a6d25ca88759cc7565ae1
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-07-20 00:18:57 +0530

    add title-logo update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Newcrops from './components/Newcrops'
@@ -7,7 +5,6 @@ import News from './components/News'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
