17e32b160ecca93ccdd73c6de587d731feb4d4cf
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-07-20 00:13:07 +0530

    add small change in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,25 @@ function Footer() {
         console.log("Form Submited");
     }
   return (
-    <div className='w-screen flex items-center justify-between rounded-t-full py-6 px-20 bg-green-700 text-white'>
-        <div>
-            <p>a product by CODINGAASHARAM COMMUNITY</p>
-            <h3 className='text-lg text-lime-300 mt-3'>For Daily Updates Subscribe By Submitting Your Email Below.</h3>
-            <form 
-            onSubmit={event => handleSubmit(event)}
-            className='mt-1 flex items-center gap-5'>
-                <input 
-                type="email" 
-                placeholder='example2gmail.com'
-                className='px-5 py-2 rounded-3xl text-lime-600'/>
-                <button 
-                type='submit' 
-                className='px-3 py-2 bg-lime-400 rounded-3xl'>Submit</button>
-            </form>
+    <div className='w-screen rounded-t-full py-6 px-20 bg-green-700 text-white'>
+        <p className='text-center text-lg font-semibold'>a product by CODINGAASHARAM COMMUNITY</p>
+        <div className='w-full flex items-center justify-between'>
+            <div>
+                <h3 className='text-lg text-lime-300 mt-3'>For Daily Updates Subscribe By Submitting Your Email Below.</h3>
+                <form 
+                onSubmit={event => handleSubmit(event)}
+                className='mt-1 flex items-center gap-5'>
+                    <input 
+                    type="email" 
+                    placeholder='example2gmail.com'
+                    className='px-5 py-2 rounded-3xl text-lime-600'/>
+                    <button 
+                    type='submit' 
+                    className='px-3 py-2 bg-lime-400 rounded-3xl'>Submit</button>
+                </form>
+            </div>
+            <a href="/" className='text-xl font-semibold bg-lime-200 text-black px-5 py-2 rounded-3xl border-2 border-white hover:bg-lime-400'>GitHub</a>
         </div>
-        <a href="/" className='text-xl font-semibold bg-lime-200 text-black px-5 py-2 rounded-3xl border-2 border-white hover:bg-lime-400'>GitHub</a>
     </div>
   )
 }
