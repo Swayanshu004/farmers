96d4f881dc97a42a4c1bf100027649ce4d43f254
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-12-16 23:25:55 +0530

    update landing page

--- src/components/Newcrops.jsx
+++ src/components/Newcrops.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Newcrops() {
   return (
     <div className='bg-neutral-300 py-10 flex flex-wrap rounded-xl justify-center mx-10'>
-        <div className='w-52 h-fit flex flex-col items-center justify-center bg-green-700 rounded-lg py-5 m-16'>
+        <div className='w-52 h-fit flex flex-col items-center justify-center bg-green-700 rounded-lg py-5 mx-10 my-5'>
             <img src="https://images.pexels.com/photos/21581354/pexels-photo-21581354/free-photo-of-men-driving-a-tractor-with-harvest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="na" 
             className='w-44 rounded-xl'/>
             <h2 className='text-xl font-bold'>Modern Farming</h2>
@@ -11,7 +11,7 @@ function Newcrops() {
             <a href="https://blog.google/around-the-globe/google-asia/jiva-25/"
             className='text-md font-bold bg-lime-300 px-3 py-1 mt-5 rounded-3xl border-2 border-black hover:bg-lime-400'>SHOW</a>
         </div>
-        <div className='w-52 h-fit flex flex-col items-center justify-center bg-green-700 rounded-lg py-5 m-16'>
+        <div className='w-52 h-fit flex flex-col items-center justify-center bg-green-700 rounded-lg py-5 mx-10 my-5'>
             <img src="https://images.pexels.com/photos/21581354/pexels-photo-21581354/free-photo-of-men-driving-a-tractor-with-harvest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="na" 
             className='w-44 rounded-xl'/>
             <h2 className='text-xl font-bold'>Modern Farming</h2>
@@ -19,7 +19,7 @@ function Newcrops() {
             <a href="https://timesofindia.indiatimes.com/india/yogi-govt-prioritizes-farmer-prosperity-with-focus-on-horticulture-and-vegetable-farming/articleshow/111867617.cms"
             className='text-md font-bold bg-lime-300 px-3 py-1 mt-5 rounded-3xl border-2 border-black hover:bg-lime-400'>SHOW</a>
         </div>
-        <div className='w-52 h-fit flex flex-col items-center justify-center bg-green-700 rounded-lg py-5 m-16'>
+        <div className='w-52 h-fit flex flex-col items-center justify-center bg-green-700 rounded-lg py-5 mx-10 my-5'>
             <img src="https://images.pexels.com/photos/916406/pexels-photo-916406.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpxl" alt="na" 
             className='w-44 rounded-xl'/>
             <h2 className='text-xl font-bold'>Modern Farming</h2>
@@ -27,7 +27,7 @@ function Newcrops() {
             <a href="/"
             className='text-md font-bold bg-lime-300 px-3 py-1 mt-5 rounded-3xl border-2 border-black hover:bg-lime-400'>SHOW</a>
         </div>
-        <div className='w-52 h-fit flex flex-col items-center justify-center bg-green-700 rounded-lg py-5 m-16'>
+        <div className='w-52 h-fit flex flex-col items-center justify-center bg-green-700 rounded-lg py-5 mx-10 my-5'>
             <img src="https://images.pexels.com/photos/21581354/pexels-photo-21581354/free-photo-of-men-driving-a-tractor-with-harvest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="na" 
             className='w-44 rounded-xl'/>
             <h2 className='text-xl font-bold'>Modern Farming</h2>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,16 +4,16 @@ function News() {
   return (
     <div className='w-screen py-10 flex flex-wrap justify-center'>
         <div className='w-1/3 h-56 py-20 bg-lime-300 rounded-3xl mx-16 my-10'>
-            <h1 className='text-5xl font-bold text-center'>NEWS-1</h1>
+            <h1 className='text-2xl md:text-5xl font-bold text-center'>NEWS-1</h1>
         </div>
         <div className='w-1/3 h-56 py-20 bg-gray-500 rounded-3xl mx-16 my-10'>
-            <h1 className='text-5xl font-bold text-center'>NEWS-2</h1>
+            <h1 className='text-2xl md:text-5xl font-bold text-center'>NEWS-2</h1>
         </div>
         <div className='w-1/3 h-56 py-20 bg-gray-500 rounded-3xl mx-16 my-10'>
-            <h1 className='text-5xl font-bold text-center'>NEWS-3</h1>
+            <h1 className='text-2xl md:text-5xl font-bold text-center'>NEWS-3</h1>
         </div>
         <div className='w-1/3 h-56 py-20 bg-lime-300 rounded-3xl mx-16 my-10'>
-            <h1 className='text-5xl font-bold text-center'>NEWS-4</h1>
+            <h1 className='text-2xl md:text-5xl font-bold text-center'>NEWS-4</h1>
         </div>
     </div>
   )
