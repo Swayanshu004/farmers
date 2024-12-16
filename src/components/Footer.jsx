c0a716da69eb13cd391cb65a628a992b302533a5
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-12-16 23:27:25 +0530

    update landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
     <div className='w-auto rounded-2xl py-6 px-10 md:px-20 bg-green-700 text-white'>
         <div className='w-auto gap-2 md:gap-0 flex flex-col md:flex-row items-center justify-between'>
             <div>
-                <h3 className='text-lg text-lime-300 mt-3'>For Daily Updates Subscribe our news letter : </h3>
+                <h3 className='text-base md:text-lg text-lime-300 mt-3'>For Daily Updates Subscribe our news letter : </h3>
                 <form 
                 onSubmit={event => handleSubmit(event)}
                 className='mt-1 flex items-center gap-5'>
@@ -25,7 +25,7 @@ function Footer() {
             <hr />
             <a href="/" className='text-xl font-semibold bg-lime-400 text-black px-5 py-2 rounded-3xl border-2 border-black hover:bg-lime-500'>GitHub</a>
         </div>
-        <p className='text-center text-lg font-semibold mt-3'>a product by CODINGAASHARAM COMMUNITY</p>
+        <p className='text-neutral-200 text-center text-lg font-semibold mt-3'>a product by CODINGAASHARAM COMMUNITY</p>
     </div>
   )
 }
