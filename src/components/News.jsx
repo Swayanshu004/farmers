865985f949c49386906ec06c1c0853ef7628cd21
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-12-23 11:11:01 +0530

    update landing page

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 function News() {
   return (
     <div className='w-screen py-10 flex flex-wrap justify-center'>
-        <div className='w-1/3 h-56 py-20 bg-lime-300 rounded-3xl mx-16 my-10'>
+        <div className='w-full md:w-1/3 h-56 py-20 bg-lime-300 rounded-3xl mx-10 my-5'>
             <h1 className='text-2xl md:text-5xl font-bold text-center'>NEWS-1</h1>
         </div>
-        <div className='w-1/3 h-56 py-20 bg-gray-500 rounded-3xl mx-16 my-10'>
+        <div className='w-full md:w-1/3 h-56 py-20 bg-gray-500 rounded-3xl mx-10 my-5'>
             <h1 className='text-2xl md:text-5xl font-bold text-center'>NEWS-2</h1>
         </div>
-        <div className='w-1/3 h-56 py-20 bg-gray-500 rounded-3xl mx-16 my-10'>
+        <div className='w-full md:w-1/3 h-56 py-20 bg-gray-500 rounded-3xl mx-10 my-5'>
             <h1 className='text-2xl md:text-5xl font-bold text-center'>NEWS-3</h1>
         </div>
-        <div className='w-1/3 h-56 py-20 bg-lime-300 rounded-3xl mx-16 my-10'>
+        <div className='w-full md:w-1/3 h-56 py-20 bg-lime-300 rounded-3xl mx-10 my-5'>
             <h1 className='text-2xl md:text-5xl font-bold text-center'>NEWS-4</h1>
         </div>
     </div>
